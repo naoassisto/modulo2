8e5941db6a9c658cb58e939d99b5f7dbfae8d522
naoassisto <99260189+naoassisto@users.noreply.github.com>
2022-06-21 07:09:39 -0300

    Ajustes Pendencias, Curriculos atrasados

--- 03_AUT_EST_ENTREGA/Semana 5/Curriculo/Controle/contro.js
+++
@@ -1,8 +0,0 @@
-import { openDb } from '../curri';
-
-export async function createTable () {
-    openDb().then(db=>{
-        db.exec('CREATE TABLE IF NOT EXISTS curriculo (id INTEGER PRIMARY KEY, formacao TEXT, experiencia TEXT, idiomas text)');
-    })
-}
-
--- 03_AUT_EST_ENTREGA/Semana 5/Curriculo/Controle/curri.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE curriculo(  
-    id NOT NULL PRIMARY KEY, formacao TEXT,
-    experiencia TEXT, idiomas TEXT
-);
-
-SELECT * FROM curriculo LIMIT 100;
-
-
-
